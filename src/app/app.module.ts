a93017572f00301b87a5629ce1dc4b76426a4514
Galnykin Dmitriy <developer085@gmail.com>
2017-01-09 07:40:15 +0300

    basic router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import {AppComponent} from './app.component';
 import {MainMenuComponent} from './main-menu/main-menu.component';
 
 import {Page0Component as RootPageComponent} from './page0/page0.component';
+import {Page1Component} from './page1/page1.component';
+import {Page2Component} from './page2/page2.component';
+import {Page3Component} from './page3/page3.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {BodyComponent} from './body/body.component';
 import {AnyPageComponent} from './any-page/any-page.component';
@@ -39,6 +42,9 @@ export class ItemResolver implements Resolve<any> {
 
 export const appRoutes: Routes = [
   {path: '',component: RootPageComponent},
+  {path: 'page-1',component: Page1Component},
+  {path: 'page-2',component: Page2Component},
+  {path: 'page-3',component: Page3Component},
   {
     path: ':path',
     component: AnyPageComponent,
@@ -54,6 +60,9 @@ export const appRoutes: Routes = [
   declarations: [
     AppComponent,
     RootPageComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
     MainMenuComponent,
     NotFoundComponent,
     BodyComponent,
