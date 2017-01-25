bfbd9d12b34c969d5257d62b0c48867c9a9fa8fe
Galnykin Dmitriy <developer085@gmail.com>
2017-01-25 04:20:37 +0300

    very basic start

--- src/app/any-page/any-page.component.ts
+++ src/app/any-page/any-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef  } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
