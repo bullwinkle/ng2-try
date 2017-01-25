0f188445811d9f05457e309197219000275331d1
Galnykin Dmitriy <developer085@gmail.com>
2017-01-25 04:20:37 +0300

    very basic start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
-import {NgModule, Compiler, Injectable} from '@angular/core';
-import {HttpModule, Http} from '@angular/http';
-import {RouterModule, Routes, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {NgModule, Compiler} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {Observable} from 'rxjs';
-import 'rxjs/add/operator/map';
 
 import {AppComponent} from './app.component';
 import {MainMenuComponent} from './main-menu/main-menu.component';
@@ -15,47 +13,8 @@ import {Page2Component} from './page2/page2.component';
 import {Page3Component} from './page3/page3.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {BodyComponent} from './body/body.component';
-import {AnyPageComponent} from './any-page/any-page.component';
-import { BodyInnerComponent } from './body/body-inner/body-inner.component';
 
-
-@Injectable()
-export class ItemResolver implements Resolve<any> {
-
-  http: Http;
-
-  constructor(http: Http) {
-    this.http = http;
-  }
-
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot,
-  ): Observable<any>|Promise<any>|any {
-
-    console.warn('route,state', route, state)
-
-    return this.http.get('/page.html')
-      .map(res=>res.text())
-  }
-}
-
-
-export const appRoutes: Routes = [
-  {path: '',component: RootPageComponent},
-  {path: 'page-1',component: Page1Component},
-  {path: 'page-2',component: Page2Component},
-  {path: 'page-3',component: Page3Component},
-  {
-    path: ':path',
-    component: AnyPageComponent,
-    resolve: {
-      item: ItemResolver
-    }
-  },
-  {path: '**',component: NotFoundComponent},
-];
-
+import {appRoutes, ItemResolver} from './app.router';
 
 @NgModule({
   declarations: [
@@ -66,9 +25,7 @@ export const appRoutes: Routes = [
     Page3Component,
     MainMenuComponent,
     NotFoundComponent,
-    BodyComponent,
-    AnyPageComponent,
-    BodyInnerComponent
+    BodyComponent
   ],
   imports: [
     BrowserModule,
--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { AnyPageComponent } from './any-page.component';
+import { SidebarComponent } from './sidebar.component';
 
-describe('AnyPageComponent', () => {
-  let component: AnyPageComponent;
-  let fixture: ComponentFixture<AnyPageComponent>;
+describe('SidebarComponent', () => {
+  let component: SidebarComponent;
+  let fixture: ComponentFixture<SidebarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AnyPageComponent ]
+      declarations: [ SidebarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AnyPageComponent);
+    fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
