d53100538382c732e7564ccbde31dfa5c378bf95
Galnykin Dmitriy <developer085@gmail.com>
2017-01-25 11:42:11 +0300

    router refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import {NgModule, Compiler} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
-import {RouterModule} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {MainMenuComponent} from './main-menu/main-menu.component';
+import {AppRouterModule} from './app.router';
 
 import {Page0Component as RootPageComponent} from './page0/page0.component';
 import {Page1Component} from './page1/page1.component';
 import {Page2Component} from './page2/page2.component';
 import {Page3Component} from './page3/page3.component';
 import {NotFoundComponent} from './not-found/not-found.component';
-import {BodyComponent} from './body/body.component';
-
-import {appRoutes, ItemResolver} from './app.router';
 
 @NgModule({
   declarations: [
@@ -24,20 +21,14 @@ import {appRoutes, ItemResolver} from './app.router';
     Page2Component,
     Page3Component,
     MainMenuComponent,
-    NotFoundComponent,
-    BodyComponent
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  providers: [
-    Compiler,
-    ItemResolver
+    AppRouterModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,8 +1,5 @@
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import {Routes, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
-import {Observable} from 'rxjs';
-import 'rxjs/operator/map';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import {Page0Component as RootPageComponent} from './page0/page0.component';
 import {Page1Component} from './page1/page1.component';
@@ -10,31 +7,35 @@ import {Page2Component} from './page2/page2.component';
 import {Page3Component} from './page3/page3.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 
-@Injectable()
-export class ItemResolver implements Resolve<any> {
-
-  http: Http;
-
-  constructor(http: Http) {
-    this.http = http;
-  }
-
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot,
-  ): Observable<any>|Promise<any>|any {
-
-    console.warn('route,state', route, state)
-
-    return this.http.get('/page.html')
-      .map(res=>res.text())
-  }
-}
-
-export const appRoutes: Routes = [
-  {path: '',component: RootPageComponent},
-  {path: 'page-1',component: Page1Component},
-  {path: 'page-2',component: Page2Component},
-  {path: 'page-3',component: Page3Component},
-  {path: '**',component: NotFoundComponent},
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: RootPageComponent
+  },
+  {
+    path: 'page-1',
+    component: Page1Component
+  },
+  {
+    path: 'page-2',
+    component: Page2Component
+  },
+  {
+    path: 'page-3',
+    component: Page3Component
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
 ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRouterModule {}
